99929bc77c9fb31811895e517d562dac13744086
Axel Arcaya <ganinox@gmail.com>
2023-04-16 22:42:24 -0400

    Actualizacion de metodos cerrar y abrir puertas.

--- SistemaDeControlInteligente/LectorRFID.cs
+++ SistemaDeControlInteligente/LectorRFID.cs
@@ -18,20 +18,50 @@ public class LectorRFID
         }
     }
 
-    public void AbrirPuerta()
+
+    public void AbrirPuerta(string Fuente)
     {
-        //Aqui se introduce el codigo de apertura de la puerta, dependiendo de el modelo del controlador o las librerias usadas este puede cambiar.
-        //para efectos de demostracion solo se escribe en consola un mensaje de apertura.
-        Console.WriteLine("Abriendo puerta...");
-        this.LogOperationes("ABIERTO");
+        if (Autorizacion(Fuente))
+        {
+            //Aqui se introduce el codigo de apertura de la puerta, dependiendo de el modelo del controlador o las librerias usadas este puede cambiar.
+            //para efectos de demostracion solo se escribe en consola un mensaje de apertura.
+            Console.WriteLine("Abriendo puerta...");
+            this.LogOperationes("ABIERTO");
+        }
+        else
+        {
+            Console.WriteLine("Access denied.");
+        }
     }
 
-    public void CerrarPuerta()
+    public void CerrarPuerta(string Fuente)
     {
-        //Aqui se introduce el codigo de cierre de la puerta, dependiendo de el modelo del controlador o las librerias usadas este puede cambiar.
-        //para efectos de demostracion solo se escribe en consola un mensaje de cierre.
-        Console.WriteLine("Cerrando puerta...");
-        this.LogOperationes("Cerrado");
+        if (Autorizacion(Fuente))
+        {
+            //Aqui se introduce el codigo de cierre de la puerta, dependiendo de el modelo del controlador o las librerias usadas este puede cambiar.
+            //para efectos de demostracion solo se escribe en consola un mensaje de cierre.
+            Console.WriteLine("Cerrando puerta...");
+            this.LogOperationes("Cerrado");
+        }
+        else
+        {
+            Console.WriteLine("Access denied.");
+        }
+    }
+
+    private bool Autorizacion(string Fuente)
+    {
+        string[] authorizedCodes = File.ReadAllLines(Fuente);
+        // leer archivo con códigos autorizados
+        string currentCode = "RFID_code"; // leer código RFID leído por el lector
+        foreach (string code in authorizedCodes)
+        {
+            if (code == currentCode)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void LogOperationes(string operation)
